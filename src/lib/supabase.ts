3406a4b690716c8ebaf0798a9ac491d6481be8a9
Hugues Frantz <89164291+TheLastpressedkey@users.noreply.github.com>
2025-03-20 23:21:45 +0100

    Add files via upload

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://tppldbgypnxybwcqyoyd.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwcGxkYmd5cG54eWJ3Y3F5b3lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI0MjQwMjUsImV4cCI6MjA1ODAwMDAyNX0.AveYYilJOBxG-6iq6ljLoTYoQu13x2VUgJI2PvlvmN0';
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
+  throw new Error('Missing Supabase configuration');
 }
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
